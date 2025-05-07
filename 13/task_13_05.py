2179fd207197b1bc08ddd3370ebf2dbcccca9649
evgeniy-kulikov <redstepway@gmail.com>
2025-05-07 22:41:11 +0300

    task_13

--- 13/task_13_05.py
+++ 13/task_13_05.py
@@ -4,3 +4,168 @@ Task 13
 ЕГЭ Питоныч
 https://stepik.org/course/100138
 """
+
+""" 25.1 Основы IP-адресации """
+
+# https://stepik.org/lesson/1190361/step/3?unit=1203331
+b = '11000000.10101000.00000101.00010011'
+d = [str(int(i, 2)) for i in b.split('.')]
+print('.'.join(d))  # 192.168.5.19
+
+
+# https://stepik.org/lesson/1190361/step/12?unit=1203331
+from ipaddress import ip_network
+print(ip_network('208.128.193.64/255.255.224.0', 0))
+# 208.128.192.0/19
+
+
+
+""" 25.2 Решение задач. Часть 1 """
+
+# https://stepik.org/lesson/1190362/step/2?unit=1203332
+from ipaddress import ip_network
+
+cnt = 0
+net = ip_network('156.128.0.227/255.255.255.248', 0)  # 156.128.0.224
+for i in net.hosts():
+    cnt += 1
+    if str(i) == '156.128.0.227':
+        print(cnt, i)  # 3  156.128.0.227
+
+# https://stepik.org/lesson/1190362/step/7?unit=1203332
+from ipaddress import *
+for m in range(17, 33):
+    net = ip_network(f'148.228.120.242/{m}', 0)
+    if str(net.network_address) == '148.228.112.0':
+        m_3 = ('1' * m).ljust(32, '0')[16:24]
+        print(m_3)  # 11110000
+        print(int(m_3, 2))  # 240
+
+# конвертирование десятичного IP адреса в двоичный
+def bin_ip(s: str):
+    return '.'.join(f'{int(i):b}'.zfill(8) for i in s.split('.'))
+host = '148.228.120.242'
+net = '148.228.112.0'
+print(bin_ip(host))
+print(bin_ip(net))
+"""
+10010100.11100100.01111000.11110010       # host
+11111111.11111111.11110000.00000000       # 11110000 --> 240
+10010100.11100100.01110000.00000000       # net
+"""
+
+
+# https://stepik.org/lesson/1190362/step/8?unit=1203332
+from ipaddress import *
+for m in range(17, 33):
+    net = ip_network(f'153.209.31.240/{m}', 0)
+    if str(net.network_address) == '153.209.28.0':
+        m_3 = ('1' * m).ljust(32, '0')[16:24]
+        print(m_3)  # 11111100
+        print(int(m_3, 2))  # 252
+
+
+# https://stepik.org/lesson/1190362/step/12?unit=1203332
+from ipaddress import *
+net = ip_network('64.128.194.208/255.255.240.0', 0)
+print(net)  # 64.128.192.0/20
+
+
+""" 25.3 Решение задач. Часть 2 """
+
+# https://stepik.org/lesson/1190363/step/2?unit=1203333
+from ipaddress import *
+for m in range(33):
+    net = ip_network(f'148.228.120.242/{m}', 0)
+    if str(net.network_address) == '148.228.112.0':
+        # print(net.netmask)  # 255.255.240.0
+        print(str(net.netmask).split('.')[2])  # 240
+        break
+
+
+# https://stepik.org/lesson/1190363/step/7?unit=1203333
+from ipaddress import *
+for m in range(33):
+    net = ip_network(f'68.112.69.138/{m}', 0)
+    if str(net.network_address) == '68.112.64.0':
+        print(m)
+# 21
+
+
+# https://stepik.org/lesson/1190363/step/10?unit=1203333
+from ipaddress import *
+for m in range(33):
+    net = ip_network(f'15.51.208.15/{m}', 0)
+    if str(net.network_address) == '15.51.192.0':
+        msk = ('1' * m).ljust(32, '0')
+        print(int(msk[16:24], 2))
+# 224
+
+
+""" 25.4 Решение задач. Часть 3 """
+
+# https://stepik.org/lesson/1192962/step/2?unit=1205951
+from ipaddress import *
+for m in range(33):
+    net_1 = ip_network(f'112.117.107.70/{m}', 0)
+    net_2 = ip_network(f'112.117.121.80/{m}', 0)
+    if net_1 == net_2:
+        # print(net_1.netmask)
+        print(str(net_1.netmask).split('.')[2])
+# 224
+
+
+# https://stepik.org/lesson/1192962/step/6?unit=1205951
+from ipaddress import *
+for m in range(33):
+    net_1 = ip_network(f'151.172.115.121/{m}', 0)
+    net_2 = ip_network(f'151.172.115.156/{m}', 0)
+    if net_1 != net_2:
+        print(m)  # 25
+        break
+
+
+# https://stepik.org/lesson/1192962/step/11?unit=1205951
+from ipaddress import *
+net = ip_network('1.2.3.0/255.255.255.0', 0)
+print(net.num_addresses - 2)  # 254
+# 256 - 2 = 254
+
+
+""" 25.5 Решение задач. Часть 4 """
+
+# https://stepik.org/lesson/1192963/step/2?unit=1205952
+from ipaddress import *
+cnt = 0
+net = ip_network('172.16.128.0/255.255.192.0', 0)
+for n in net:
+    cnt += f'{n:b}'.count('1') % 2
+print(cnt)  # 8192
+
+
+# https://stepik.org/lesson/1192963/step/7?unit=1205952
+from ipaddress import *
+cnt = 0
+net = ip_network('192.168.248.176/255.255.255.240', 0)
+for n in net:
+    cnt += f'{n:b}'.count('1') > 16
+print(cnt)  # 1
+
+
+# https://stepik.org/lesson/1192963/step/8?unit=1205952
+from ipaddress import *
+cnt = 0
+net = ip_network('211.48.136.64/255.255.255.224', 0)
+for n in net:
+    cnt += f'{n:b}'[-2:] == '11'
+print(cnt)  # 8
+
+
+# https://stepik.org/lesson/1192963/step/11?unit=1205952
+from ipaddress import *
+cnt = 0
+net = ip_network('140.19.96.0/255.255.248.0', 0)
+for n in net:
+    ls = [f'{n:b}'[i:i + 8].count('1') for i in range(0, 32, 8)]
+    cnt += len(set(ls)) == 1
+print(cnt)  # 168
--- 13/task_13_FAQ.py
+++ 13/task_13_FAQ.py
@@ -1,25 +1,62 @@
 """"""
 
 """
+хост (узел)  адрес комрьютера в сети
+маска  - слева единицы, справа нули
+адрес сети - количество нулей (двоичных) справа определяет количество узлов внутри сети
+
 
 ip_address(address)
 Take an IP string/int and return an object of the correct type.
 
 ip_network(address, strict=True):
 Take an IP string/int and return an object of the correct type.
+ip_network('112.192.1.1', 0)    аргументы: [адрес хоста/сети],  [нестрогая проверка (ошибки игнорируются)]
+Функция print() выводит сеть вместе с маской (кол-во единиц)
+print( ip_network('64.128.194.208/255.255.240.0', 0))  # 64.128.192.0/20
+
+Итерация по ip_network выдает объект, а не строку
+net = ip_network('64.128.194.208/255.255.240.0', 0)
+for n in net:
+    a = str(n)       # 64.128.194.208
+    b = int(n)       # 4656757
+    c = f'{n}:b'     # 1001010110100101010101
+
 
 ip_network(address).network_address
-формирование IP адреса (маски в записи нет)
+формирование IP адреса (маски в записи нет) в виде обЪекта (со строкой не сравнить)
+str(ip_network(address).network_address)  формирование IP адреса в виде строки
 
+ip_network(address).netmask
+формирование адреса маски в виде обЪекта (со строкой не сравнить)
 
 ip_network(address).hosts()
 Generate Iterator over usable hosts in a network. 
 it doesn't return the network or broadcast addresses
 
+ip_network(address).num_addresses
+Number of hosts in the current subnet
+возвращает количество возможных номеров компьютеров в сети (включая адрес сети и широковешательный)
+
 """
+
+
 """
+десятичное в двоичное
+f'{8:b}'  -->  1000
+bin(8)[2:]  -->  1000
+
+двоичное в десятичное
+int('1000', 2)  --> 8
+
+
+
 '11'.ljust(8, '0')  -->  '11000000'
 
 '11'.rjust(8, '0')  -->  '00000011'
 '11'.zfill(8)  -->  '00000011'
 """
+
+# конвертирование десятичного IP адреса в двоичный
+def bin_ip(s: str):
+    return '.'.join(f'{int(i):b}'.zfill(8) for i in s.split('.'))
